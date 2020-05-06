20d488199b201260c510ae79715fbfe1ebc7e13d
David Martinez <davymtz91@gmail.com>
2020-05-05 21:19:39 -0500

    Se terminaron diseños y estilos de las secciones

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -8,9 +8,11 @@
     <title>OROGOLD | Test</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('assets/css/index.css') }}">
+    <link rel="stylesheet" href="{{ asset('assets/css/owl.carousel.css') }}">
+    <!--link rel="stylesheet" href="{{ asset('assets/css/owl.theme.default.css') }}"-->
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark" style="padding: 1em 10%;">
+    <nav class="navbar navbar-expand-lg" style="padding: 1em 10%; background: black;">
         <a class="navbar-brand" href="#">
             <img src="{{ asset('assets/images/cropped-orogold-logo-dgold.png') }}" width="150" height="auto" class="d-inline-block align-top" alt="image">
         </a>
@@ -22,7 +24,7 @@
             <ul class="navbar-nav ml-auto">
                 <li>
                     <span class="navbar-text orogold-text">
-                      orogoldspa@orogold.com
+                        orogoldspa@orogold.com
                     </span>
                 </li>
             </ul>
@@ -34,8 +36,60 @@
 
     @yield('content')
 
+    <nav class="navbar nav_footer" style="padding: 1em 10%; background: #000;">
+        <a class="navbar-brand" href="#">
+            <img src="{{ asset('assets/images/cropped-orogold-logo-dgold.png') }}" width="150" height="auto" class="d-inline-block align-top" alt="image">
+        </a>
+        <div class="row">
+            <span class="navbar-text orogold-text">
+                <img width="20" height="auto" src="{{ asset('assets/icon/mail.png') }}" alt=""> orogoldspa@orogold.com
+            </span>
+            <span class="navbar-text orogold-text">
+                <img width="20" height="auto" src="{{ asset('assets/icon/telephone.png') }}" alt=""> + 52 5502630032
+            </span>
+        </div>
+    </nav>
+
 </body>
 <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+<script src="{{asset('assets/js/owl.carousel.js')}}"></script>
+<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCKGEgHdBqgn9xE6IFPwpbbjUlBgO2mDFI&callback=initMap" async defer></script>
+<script>
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        nav:false,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause:true,
+        autoHeight:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:5
+            }
+        }
+    });
+    // Mapas
+    var map;
+    function initMap(){
+        map =  new google.maps.Map(document.getElementById('map'), {
+            center: {lat: 19.425687, lng: -99.162341},
+            zoom: 13
+        });
+        var marker;
+        marker = new google.maps.Marker({
+            position: {lat: 19.425687, lng: -99.162341},
+            map: map,
+            title: "Test de prueba"
+        });
+    }
+</script>
 </html>
--- resources/views/sections/first_section.blade.php
+++ resources/views/sections/first_section.blade.php
@@ -29,7 +29,7 @@
         </div>
     </section>
     <section class="section second_section">
-        <div class="container" style="padding: 2em 0;">
+        <div class="container" style="padding: 1em 0;">
             <div class="row text-center justify-content-center">
                 <h1 class="title">NUESTROS TRATAMIENTOS</h1>
                 <h5 class="subtitle">
@@ -43,6 +43,7 @@
                         <img width="150" height="auto" src="{{asset('assets/images/photo-1516238840914-94dfc0c873ae.jpg')}}" alt="masajes de oro">
                     </div>
                     <h5 class="card-title">MASAJES DE ORO</h5>
+                    <br>
                     <p>
                         <strong>SPA Orogold</strong> ofrece tratamientos especializados de la mano de nuestros
                         profesionales expertos, con productos excepcionales de marcas galardonadas.
@@ -68,6 +69,84 @@
                         a un oasis de relajación. Incluye un masaje de espalda, hombros, piernas y pies.
                     </p>
                 </div>
+            </div>
+        </div>
+    </section>
+    <section class="section third_section" style="background-image: url('{{asset('assets/images/photo-1520473378652-85d9c4aee6cf.jpg')}}');">
+        <div class="transbox">
+            <div class="container-fluid">
+                <div class="row flex-column justify-content-center align-items-center" data-column="first">
+                    <h1>QUIENES SOMOS</h1>
+                    <p>El lujoso spa en Orogold, en México, ofrece un refugio sumblime en pleno corazón de la ciudad</p>
+                    <p>Los huéspedes pueden disfrutar de un gimnasio de última generación, una relajante sauna o duchas
+                        a chorro durante su escapada de 5 estrellas en nuestro hotel boutique en París. Por otra parte, nuestra
+                        nueva asociación con ZEAL COSMETICS asegura la disponibilidad de una gama de relajantes tratamientos faciales
+                        y corporales para mimarse y revitalizarse.</p>
+                </div>
+                <div class="row">
+                    <div class="owl-carousel">
+                        <img width="auto" height="220px" src="{{asset('assets/images/photo-1553540751-988bd7918c7e.jpg')}}">
+                        <img width="auto" height="220px" src="{{asset('assets/images/photo-1554057009-6798cb3d4a04.jpg')}}">
+                        <img width="auto" height="220px" src="{{asset('assets/images/photo-1560944527-a4a429848866.jpg')}}">
+                        <img width="auto" height="220px" src="{{asset('assets/images/photo-1489659639091-8b687bc4386e.jpg')}}">
+                        <img width="auto" height="220px" src="{{asset('assets/images/photo-1513104806186-1b00f3e7a65e.jpg')}}">
+                        <img width="auto" height="220px" src="{{asset('assets/images/photo-1531853121101-cb94c8ed218d.jpg')}}">
+                        <img width="auto" height="220px" src="{{asset('assets/images/photo-1555820585-c5ae44394b79.jpg')}}">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="fourth_section" style="background-color: #F8F8F8;">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <div id="map" style="width: 700px; height: 350px;"></div>
+                </div>
+                <div class="col-md-4" data-column="second">
+                    <div class="row">
+                        <div class="col">
+                            <h5>HORARIOS</h5>
+                            <p>
+                                Lunes a viernes de 10:00 a 19:30 <br>
+                                Sábados, domingos y festivos de 10:00 a 18:30.
+                            </p>
+                            <p>
+                                Pueden concertarse tratamientos fuera del horario de apertura indicado
+                                con cita previa.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <h5>DIRECCIÓN</h5>
+                            <p>
+                                Av. Insurgentes Sur 105, Roma Sur, Cuahutémoc, 06600 Ciudad de México,
+                                CDMX, México
+                            </p>
+                            <button class="btn btn-outline-orogold_map">Ver en Mapa</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section class="fifth_section">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <img src="{{asset('assets/images/photo-1489659639091-8b687bc4386e.jpg')}}" alt="Imagen de test">
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <h5>NO DEJES PASAR ESTA OPORTUNIDAD</h5>
+                <p class="text-center">
+                    Sabías que un masaje debido a su acción calmante y análgesica viene dada mediante actos reflejos
+                    que afectan al sistema nervioso central o esto causa liberación de endorfinas que anulan las sensaciones
+                    de dolor en el cerebro. El estímulo de los mecanos receptores mediante el masaje se ha demostrado
+                    que alivia el dolor y la tensión muscular.
+                </p>
+                <button class="btn btn-reservar">Reserva ahora</button>
             </div>
         </div>
     </section>
